5b1f1e8a39775adfa28cce2ac914b541652a7253
Sulthon <trooulala@gmail.com>
2022-05-30 16:29:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ st.write(
 
     Selamat datang, silahkan lakukan chek validasi kata dengan menginputkan kata yang ada dalam daftar.
     Berikut adalah daftar kata yang bisa dichek pada lexical analyzer ini:
-    kulo, sampeyan, dheweke, awakdhewe, saged, budhal, gawe, saiki
+
+    kulo, sampeyan, dheweke, awakdhewe, gawe, budhal, saged, saiki, pasar, sawah
     """
 )
 
@@ -64,10 +65,10 @@ transition_table[('q6', 'a')]= 'q7'
 transition_table[('q7', 'n')]= 'q33'
 
 #update the transition table for the following token: gawe
-transition_table[('q0', 'g')]= 'q30'
-transition_table[('q30', 'a')]= 'q31'
-transition_table[('q31', 'w')]= 'q32'
-transition_table[('q32', 'e')]= 'q33'
+transition_table[('q0', 'g')]= 'q29'
+transition_table[('q29', 'a')]= 'q28'
+transition_table[('q28', 'w')]= 'q21'
+transition_table[('q21', 'e')]= 'q33'
 
 
 #update the transition table for the following token: budhal
@@ -127,258 +128,156 @@ transition_table[('q19', 'e')]= 'q20'
 transition_table[('q20', 'k')]= 'q21'
 transition_table[('q21', 'e')]= 'q33'
 
+#lexical analysis
+idx_char = 0
+state = 'q0'
+current_token = ' '
+while state!='accept':
+    current_char = input_string[idx_char]
+    current_token += current_char
+    state = transition_table[(state, current_char)]
+    if state=='q34':
+        st.write('current token: ', current_token, ', valid')
+        current_token =''
+    if state == 'error':
+        st.write('current token: ', current_token, ', error')
+        st.write('Maaf, kata yang anda masukan tidak valid')
+        st.write('Silahkan periksa kembali kata yang anda masukan')
+        break
+    idx_char = idx_char + 1
 
+#conclusi
+if state == 'accept':
+    st.write('semua token di input: ', kalimat, ', valid')
 
+#PARSER GRAMMAR CHEKER
+#input example
+#x = input()
+#x = str(x)
+#sentence = x
+print()
+print('-----------------------------------------------------------------------------------------------------')
+print(                    'Check validasi kata dengan analyzer lexical telah selesai                         ')
+print('                           Selanjutnya akan dichek aturan grammarnya                                 ')
+print('-----------------------------------------------------------------------------------------------------')
+tokens = kalimat.lower().split()
+tokens.append('EOS')
 
-# #update the transition table for the following token: ich
-# transition_table[('q0', 'i')]= 'q1'
-# transition_table[('q1', 'c')]= 'q2'
-# transition_table[('q2', 'h')]= 'q40'
-# transition_table[('q40', ' ')]= 'q41'
-# transition_table[('q40', '#')]= 'accept'
-# transition_table[('q41', ' ')]= 'q41'
-# transition_table[('q41', '#')]= 'accept'
+#symbols definition
+non_terminals = ['S', 'NN', 'VB', 'OB']
+terminals = ['kulo', 'sampeyan', 'awakhedewe', 'dheweke', 'gawe', 'budhal', 'saged', 'saiki', 'pasar', 'sawah', 'EOS']
 
-# #transation for new token
-# transition_table[('q41', 'i')]= 'q1'
-# transition_table[('q41', 'e')]= 'q19'
-# transition_table[('q41', 'm')]= 'q9'
-# transition_table[('q41', 'd')]= 'q3'
-# transition_table[('q41', 't')]= 'q30'
-# transition_table[('q41', 'p')]= 'q4'
-# transition_table[('q41', 'k')]= 'q24'
-# transition_table[('q41', 's')]= 'q12'
-# transition_table[('q41', 'n')]= 'q35'
+#parse table definition
+parse_table = {}
 
-# #update the transition table for the following token: du
-# transition_table[('q0', 'd')]= 'q3'
-# transition_table[('q3', 'u')]= 'q40'
+parse_table[('S', 'kulo')] = ['NN', 'VB', 'OB']
+parse_table[('S', 'sampeyan')] = ['NN', 'VB', 'OB']
+parse_table[('S', 'awakhedewe')] = ['NN', 'VB', 'OB']
+parse_table[('S', 'dheweke')] = ['NN', 'VB', 'OB']
+parse_table[('S', 'gawe')] = ['error']
+parse_table[('S', 'budhal')] = ['error']
+parse_table[('S', 'saged')] = ['error']
+parse_table[('S', 'saiki')] = ['error']
+parse_table[('S', 'pasar')] = ['error']
+parse_table[('S', 'sawah')] = ['error']
+parse_table[('S', 'EOS')] = ['error']
 
-# #update the transition table for the following token: trinke
-# transition_table[('q0', 't')]= 'q30'
-# transition_table[('q30', 'r')]= 'q31'
-# transition_table[('q31', 'i')]= 'q32'
-# transition_table[('q32', 'n')]= 'q33'
-# transition_table[('q33', 'k')]= 'q18'
-# transition_table[('q18', 'e')]= 'q40'
+parse_table[('NN', 'kulo')] = ['kulo']
+parse_table[('NN', 'sampeyan')] = ['sampeyan']
+parse_table[('NN', 'awakhedewe')] = ['error']
+parse_table[('NN', 'dheweke')] = ['error']
+parse_table[('NN', 'gawe')] = ['error']
+parse_table[('NN', 'budhal')] = ['error']
+parse_table[('NN', 'saged')] = ['error']
+parse_table[('NN', 'saiki')] = ['error']
+parse_table[('NN', 'pasar')] = ['error']
+parse_table[('NN', 'sawah')] = ['error']
+parse_table[('NN', 'EOS')] = ['error']
 
-# #update the transition table for the following token: tragen
-# transition_table[('q31', 'a')]= 'q34'
-# transition_table[('q34', 'g')]= 'q37'
+parse_table[('VB', 'kulo')] = ['error']
+parse_table[('VB', 'sampeyan')] = ['error']
+parse_table[('VB', 'awakhedewe')] = ['error']
+parse_table[('VB', 'dheweke')] = ['error']
+parse_table[('VB', 'gawe')] = ['gawe']
+parse_table[('VB', 'budhal')] = ['budhal']
+parse_table[('VB', 'saged')] = ['error']
+parse_table[('VB', 'saiki')] = ['error']
+parse_table[('VB', 'pasar')] = ['error']
+parse_table[('VB', 'sawah')] = ['error']
+parse_table[('VB', 'EOS')] = ['error']
 
-# #update the transition table for the following token: physik
-# transition_table[('q0', 'p')]= 'q4'
-# transition_table[('q4', 'h')]= 'q5'
-# transition_table[('q5', 'y')]= 'q6'
-# transition_table[('q6', 's')]= 'q7'
-# transition_table[('q7', 'i')]= 'q8'
-# transition_table[('q8', 'k')]= 'q40'
+parse_table[('OB', 'kulo')] = ['error'] 
+parse_table[('OB', 'sampeyan')] = ['error']
+parse_table[('OB', 'awakhedewe')] = ['error']
+parse_table[('OB', 'dheweke')] = ['error']
+parse_table[('OB', 'gawe')] = ['error']
+parse_table[('OB', 'budhal')] = ['error']
+parse_table[('OB', 'saged')] = ['error']
+parse_table[('OB', 'saiki')] = ['saiki']
+parse_table[('OB', 'pasar')] = ['pasar']
+parse_table[('OB', 'sawah')] = ['sawah']
+parse_table[('OB', 'EOS')] = ['error']
 
-# #update the transition table for the following token: saft
-# transition_table[('q0', 's')]= 'q12'
-# transition_table[('q12', 'a')]= 'q13'
-# transition_table[('q13', 'f')]= 'q14'
-# transition_table[('q14', 't')]= 'q40'
+#stack initialization
+stack = []
+stack.append('#')
+stack.append('S')
 
-# #update the transition table for the following token: studie
-# transition_table[('q12', 't')]= 'q15'
-# transition_table[('q15', 'u')]= 'q16'
-# transition_table[('q16', 'd')]= 'q17'
-# transition_table[('q17', 'i')]= 'q18'
-# transition_table[('q18', 'e')]= 'q40'
+#input reading initialization
+idx_token = 0
+symbol = tokens[idx_token]
 
-# #update the transition table for the following token: nahen
-# transition_table[('q0', 'n')]= 'q35'
-# transition_table[('q35', 'a')]= 'q36'
-# transition_table[('q36', 'h')]= 'q37'
-# transition_table[('q37', 'e')]= 'q38'
-# transition_table[('q38', 'n')]= 'q40'
+#parsing process
+try :
+    while (len(stack) > 0):
+        top = stack[len(stack)-1]
+        st.write('top = ', top)
+        st.write('symbol = ', symbol)
+        if top in terminals:
+            st.write('top adalah simbol terminal')
+            if top==symbol:
+                stack.pop()
+                idx_token = idx_token + 1
+                symbol = tokens[idx_token]
+                if symbol == 'EOS':
+                    st.write('isi stack: ', stack)
+                    stack.pop()
+            else:
+                st.write('error')
+                break
+        elif top in non_terminals:
+            st.write('top adalah simbol non-terminal')
+            if parse_table[(top, symbol)][0] != 'error':
+                stack.pop()
+                symbols_to_be_pushed = parse_table[(top, symbol)]
+                for i in range(len(symbols_to_be_pushed)-1,-1,-1):
+                    stack.append(symbols_to_be_pushed[i])
+            else:
+                st.write('error')
+                break
+        else:
+            st.write('error')
+            break
+        st.write('isi stack: ', stack)
+        st.write()
+except KeyError :
+    st.write('Kata tidak ditemukan dalam sistem')
 
-# #update the transition table for the following token: kleider
-# transition_table[('q0', 'k')]= 'q24'
-# transition_table[('q24', 'l')]= 'q25'
-# transition_table[('q25', 'e')]= 'q26'
-# transition_table[('q26', 'i')]= 'q27'
-# transition_table[('q27', 'd')]= 'q28'
-# transition_table[('q28', 'e')]= 'q29'
-# transition_table[('q29', 'r')]= 'q40'
-
-# #update the transition table for the following token: koche
-# transition_table[('q24', 'o')]= 'q22'
-# transition_table[('q22', 'c')]= 'q23'
-# transition_table[('q23', 'h')]= 'q18'
-# transition_table[('q18', 'e')]= 'q40'
-
-# #update the transition table for the following token: ernte
-# transition_table[('q0', 'e')]= 'q19'
-# transition_table[('q19', 'r')]= 'q20'
-# transition_table[('q20', 'n')]= 'q21'
-# transition_table[('q21', 't')]= 'q18'
-
-# #update the transition table for the following token: mais
-# transition_table[('q0', 'm')]= 'q9'
-# transition_table[('q9', 'a')]= 'q10'
-# transition_table[('q10', 'i')]= 'q11'
-# transition_table[('q11', 's')]= 'q40'
-
-# #lexical analysis
-# idx_char = 0
-# state = 'q0'
-# current_token = ' '
-# while state!='accept':
-#   current_char = input_string[idx_char]
-#   current_token += current_char
-#   state = transition_table[(state, current_char)]
-#   if state=='q40':
-#     print('current token: ', current_token, ', valid')
-#     current_token =' '
-#   if state == 'error':
-#     print('current token: ', current_token, ', error')
-#     print('Maaf, kata yang anda masukan tidak valid')
-#     print('Silahkan periksa kembali kata yang anda masukan')
-#     break;
-#   idx_char = idx_char + 1
-
-# #conclusion
-# if state == 'accept':
-#   print('semua token di input: ', sentence, ', valid')
-
-# #PARSER GRAMMAR CHEKER
-# #input example
-# #x = input()
-# #x = str(x)
-# #sentence = x
-# print()
-# print('-----------------------------------------------------------------------------------------------------')
-# print('Chek validasi kata dengan analyzer lexical telah selesai')
-# print('Selanjutnya akan dichek aturan grammarnya')
-# print('-----------------------------------------------------------------------------------------------------')
-# tokens = sentence.lower().split()
-# tokens.append('EOS')
-
-# #symbols definition
-# non_terminals = ['S', 'SB', 'VB', 'OB']
-# terminals = ['ich', 'du', 'kleider', 'physik', 'saft', 'mais', 'studie', 'trinke', 'ernte', 'koche', 'nahen', 'tragen']
-
-# #parse table definition
-# parse_table = {}
-
-# parse_table[('S', 'ich')] = ['SB', 'VB', 'OB']
-# parse_table[('S', 'du')] = ['SB', 'VB', 'OB']
-# parse_table[('S', 'kleider')] = ['error']
-# parse_table[('S', 'physik')] = ['error']
-# parse_table[('S', 'saft')] = ['error']
-# parse_table[('S', 'mais')] = ['error']
-# parse_table[('S', 'studie')] = ['error']
-# parse_table[('S', 'trinke')] = ['error']
-# parse_table[('S', 'ernte')] = ['error']
-# parse_table[('S', 'koche')] = ['error']
-# parse_table[('S', 'nahen')] = ['error']
-# parse_table[('S', 'tragen')] = ['error']
-# parse_table[('S', 'EOS')] = ['error']
-
-# parse_table[('SB', 'ich')] = ['ich']
-# parse_table[('SB', 'du')] = ['du']
-# parse_table[('SB', 'kleider')] = ['error']
-# parse_table[('SB', 'physik')] = ['error']
-# parse_table[('SB', 'saft')] = ['error']
-# parse_table[('SB', 'mais')] = ['error']
-# parse_table[('SB', 'studie')] = ['error']
-# parse_table[('SB', 'trinke')] = ['error']
-# parse_table[('SB', 'ernte')] = ['error']
-# parse_table[('SB', 'koche')] = ['error']
-# parse_table[('SB', 'nahen')] = ['error']
-# parse_table[('SB', 'tragen')] = ['error']
-# parse_table[('SB', 'EOS')] = ['error']
-
-# parse_table[('VB', 'ich')] = ['error']
-# parse_table[('VB', 'du')] = ['error']
-# parse_table[('VB', 'kleider')] = ['error']
-# parse_table[('VB', 'physik')] = ['error']
-# parse_table[('VB', 'saft')] = ['error']
-# parse_table[('VB', 'mais')] = ['error']
-# parse_table[('VB', 'studie')] = ['studie']
-# parse_table[('VB', 'trinke')] = ['trinke']
-# parse_table[('VB', 'ernte')] = ['ernte']
-# parse_table[('VB', 'koche')] = ['koche']
-# parse_table[('VB', 'nahen')] = ['nahen']
-# parse_table[('VB', 'tragen')] = ['tragen']
-# parse_table[('VB', 'EOS')] = ['error']
-
-# parse_table[('OB', 'ich')] = ['error']
-# parse_table[('OB', 'du')] = ['error']
-# parse_table[('OB', 'kleider')] = ['kleider']
-# parse_table[('OB', 'physik')] = ['physik']
-# parse_table[('OB', 'saft')] = ['saft']
-# parse_table[('OB', 'mais')] = ['mais']
-# parse_table[('OB', 'studie')] = ['error']
-# parse_table[('OB', 'trinke')] = ['error']
-# parse_table[('OB', 'ernte')] = ['error']
-# parse_table[('OB', 'koche')] = ['error']
-# parse_table[('OB', 'nahen')] = ['error']
-# parse_table[('OB', 'tragen')] = ['error']
-# parse_table[('OB', 'EOS')] = ['error']
-
-# #stack initialization
-# stack = []
-# stack.append('#')
-# stack.append('S')
-
-# #input reading initialization
-# idx_token = 0
-# symbol = tokens[idx_token]
-
-# #parsing process
-# try :
-#   while (len(stack) > 0):
-#     top = stack[len(stack)-1]
-#     print('top = ', top)
-#     print('symbol = ', symbol)
-#     if top in terminals:
-#       print('top adalah simbol terminal')
-#       if top==symbol:
-#         stack.pop()
-#         idx_token = idx_token + 1
-#         symbol = tokens[idx_token]
-#         if symbol == 'EOS':
-#           print('isi stack: ', stack)
-#           stack.pop()
-#       else:
-#         print('error')
-#         break;
-#     elif top in non_terminals:
-#       print('top adalah simbol non-terminal')
-#       if parse_table[(top, symbol)][0] != 'error':
-#         stack.pop()
-#         symbols_to_be_pushed = parse_table[(top, symbol)]
-#         for i in range(len(symbols_to_be_pushed)-1,-1,-1):
-#           stack.append(symbols_to_be_pushed[i])
-#       else:
-#         print('error')
-#         break;
-#     else:
-#       print('error')
-#       break;
-#     print('isi stack: ', stack)
-#     print()
-# except KeyError :
-#     print('Kata tidak ditemukan dalam sistem')
-
-# #conclusion
-# print()
-# if symbol == 'EOS' and len(stack) == 0:
-#   print('-----------------------------------------------------------------------------------------------------')
-#   print('Input string: ', sentence)
-#   print('Selamat!!!')
-#   print('Kalimat yang anda masukan diterima, karna sesuai Grammar')
-#   print('Terimakasih')
-#   print('-----------------------------------------------------------------------------------------------------')
-# else :
-#   print('-----------------------------------------------------------------------------------------------------')
-#   print('Error, input string: ', sentence)
-#   print('Mohon Maaf')
-#   print('Kalimat yang anda masukan tidak diterima, karna tidak sesuai Grammar')
-#   print('atau terdapat kata yang tidak valid')
-#   print('Silahkan chek kembali kalimat yang anda masukan')
-#   print('Terimakasih')
-#   print('-----------------------------------------------------------------------------------------------------')
+#conclusion
+st.write()
+if symbol == 'EOS' and len(stack) == 0:
+    st.write('-----------------------------------------------------------------------------------------------------')
+    st.write('Input string: ', kalimat)
+    st.write('Selamat!!!')
+    st.write('Kalimat yang anda masukan diterima, karna sesuai Grammar')
+    st.write('Terimakasih')
+    st.write('-----------------------------------------------------------------------------------------------------')
+else :
+    st.write('-----------------------------------------------------------------------------------------------------')
+    st.write('Error, input string: ', kalimat)
+    st.write('Mohon Maaf')
+    st.write('Kalimat yang anda masukan tidak diterima, karna tidak sesuai Grammar')
+    st.write('atau terdapat kata yang tidak valid')
+    st.write('Silahkan chek kembali kalimat yang anda masukan')
+    st.write('Terimakasih')
+    st.write('-----------------------------------------------------------------------------------------------------')
